1777a05a8d59b715e2e9f3d5ab8077db59de57c4
Daniel Gehlen <harmonia251251@gmail.com>
2023-11-22 08:48:51 -0300

    First commit

---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,10 @@
+class Pokemon {
+    constructor() {
+        this.number = null;
+        this.name = null;
+        this.types = [];
+        this.type = null;
+        this.photo = null;
+        this.stats = []; // Cada elemento é um objeto com name, base_stat e effort
+    }
+}
